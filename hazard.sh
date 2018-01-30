37b885425fd74a252494da9ab2ed251e668153e7
Dariusz Kowalczyk <darton@users.noreply.github.com>
2018-01-30 19:36:10 +0100

    Update hazard.sh

--- hazard.sh
+++ hazard.sh
@@ -24,6 +24,7 @@ HazardBindConf=/etc/named/named.conf.hazard-redirect
 #Adres ip serwera Ministerstwa Finansów na którym jest winietka informacyjna
 MFIPADDR=145.237.235.240
 
+[[ -d /opt/hazard/ ]] || mkdir /opt/hazard/
 
 # Deklaracja funkcji
 
